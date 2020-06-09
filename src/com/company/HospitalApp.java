8e40c096336c4f9222dedfa034f7149461adce91
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-09 16:36:08 +0200

    Refactor

--- src/com/company/HospitalApp.java
+++ src/com/company/HospitalApp.java
@@ -5,8 +5,9 @@ public class HospitalApp {
         Hospital hospital = new Hospital();
         hospital.add(new Doctor("Jan","Kowalski",7000,1500));
         hospital.add(new Nurse("Ania","Witczak",3500,4));
+        hospital.add(new Nurse("Joanna","Nowak",3800,8));
 
-        System.out.println("Pracownicy szpitala");
+        System.out.println("Pracownicy szpitala \n" );
         System.out.println(hospital.getInfo());
     }
 }
